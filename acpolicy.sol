48b6526bb751fbe82b6d498dedeac59d01df52b7
joshm1229 <joshm1229@gmail.com>
2018-03-11 10:22:54 -0700

    Added ACPolicy contract

---
+++ acpolicy.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.21;
+contract mortal {
+    /* Define variable owner of the type address */
+    address owner;
+
+    /* This function is executed at initialization and sets the owner of the contract */
+    function mortal() public { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() public { if (msg.sender == owner) selfdestruct(owner); }
+}
+
+contract ACPolicy is mortal {
+    /* This variable is the policy limit */
+    int acLimit;
+    
+    /* This runs when the contract is executed */
+    function update(int newLimit) private {
+        acLimit = newLimit;
+    }
+
+    /* Main function */
+    function get() public constant returns (int) {
+        return acLimit;
+    }
+}
+
